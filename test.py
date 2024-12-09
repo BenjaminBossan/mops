9d159e4d7b6bd147c01b679854197ac630b4ddb8
Benjamin Bossan <benjamin.bossan@gmail.com>
2024-12-09 17:08:17 +0100

    Adjust test a little bit

--- test.py
+++ test.py
@@ -11,9 +11,13 @@ MODELS_TO_TEST = [
 
 
 @pytest.mark.parametrize("model_id", MODELS_TO_TEST)
-@pytest.mark.parametrize("use_cache", [False, True])
-def test_inference(model_id, use_cache):
-    model = AutoModelForCausalLM.from_pretrained(model_id, use_cache=use_cache)
+@pytest.mark.parametrize("useless", [False, True])
+def test_inference(model_id, useless):
+    if useless:
+        # do nothing
+        pass
+
+    model = AutoModelForCausalLM.from_pretrained(model_id, useless):
     model.eval()
     inputs = torch.tensor([[0, 1, 2, 3, 4]])
     model.generate(inputs, do_sample=True, num_return_sequences=1)
